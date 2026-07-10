bc2c687b68ce46cb7fd62b76f9b69ebdcd1c37e5
00anshu00 <pk231795@gmail.com>
2026-07-10 21:53:08 +0530

    Add files via upload

---
+++ dsa cpp/Chapter 4 Nested Loops/alphabetSquare2.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int side;
+    cout << "Side: ";
+    cin >> side;
+    for(int i = 1; i <= side; i++) {
+        for(int j = 1; j <= side; j++) {
+            cout << (char)(j+96) << " ";
+        } 
+        cout << endl;
+}
+}
---
+++ dsa cpp/Chapter 4 Nested Loops/alphabetSquare3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int side;
+    cout << "Side: ";
+    cin >> side;
+    for(int i = 1; i <= side; i++) {
+        for(int j = 1; j <= side; j++) {
+            cout << (char)(i+64) << " ";
+        } 
+        cout << endl;
+}
+}
