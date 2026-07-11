2f507f59a588cc367eef4fafb9499fd29d68d9c0
00anshu00 <pk231795@gmail.com>
2026-07-11 08:18:27 +0530

    Add files via upload

---
+++ dsa cpp/Chapter 4 Nested Loops/starCross.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter a Number: ";
+    cin >> n;
+    if ( n % 2 != 0) {
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++) {
+            if (i ==  j || i + j == n + 1) {
+                cout << "* ";
+            }
+            else { cout << "  ";}
+        }
+        cout << endl;
+    } }
+    else {cout << "Please Enter An Odd Integer...";}
+}
--- dsa cpp/Chapter 4 Nested Loops/starSquare.cpp
+++ dsa cpp/Chapter 4 Nested Loops/starSquare.cpp
@@ -8,7 +8,7 @@ int main(){
    
     for(int i = 1; i <= side; i++) {
         for(int j = 1; j <= side; j++) {
-            cout << "* ";
+            cout << "*  ";
         } 
         cout << endl;
 }
